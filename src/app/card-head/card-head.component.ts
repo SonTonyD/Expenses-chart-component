8abe8ffc1320743f738efe5d2b1a42c7e92953a1
sontonyD <sontony.dinh@gmail.com>
2022-07-29 02:26:33 +0200

    new initial commit

---
+++ src/app/card-head/card-head.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-head',
+  templateUrl: './card-head.component.html',
+  styleUrls: ['./card-head.component.css']
+})
+export class CardHeadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
