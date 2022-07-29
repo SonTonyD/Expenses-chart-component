1c547cad7dfb6dd7639e8518a0c242022cdb188c
sontonyD <sontony.dinh@gmail.com>
2022-07-29 02:11:28 +0200

    deploy to heroku

--- expenses-chart/server.js
+++ expenses-chart/server.js
@@ -9,4 +9,4 @@ app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+ '/dist/expenses-chart/index.html'));
 })
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT|| 8080);
